6ed15d96b6145b0a39bd1dd8c0435dd23af4c51d
Debayan Biswas <dbiswas2001@gmail.com>
2024-08-23 12:33:12 +0530

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import logo from './logo.svg';
-import Header from "./Components/Header/Header.jsx"
-import VideoSection from "./Components/VideoSection/VideoSection.jsx"
-import DesignImagesSection from "./Section/DesignImages/DesignImages.jsx"
+import logo from "./logo.svg";
+import Header from "./Components/Header/Header.jsx";
+import VideoSection from "./Components/VideoSection/VideoSection.jsx";
+import DesignImagesSection from "./Section/DesignImages/DesignImages.jsx";
 
-import MotionGraphics from "./Assets/Video/motiongraphics.mp4"
-import graphicsdesign1 from "./Assets/Image/graphicsdesign1.jpg"
-import './App.css';
+import MotionGraphics from "./assets/Video/motiongraphics.mp4";
+import graphicsdesign1 from "./assets/Image/graphicsdesign1.jpg";
+import "./App.css";
 
 function App() {
   return (
-
     <div>
-        <style>
-        @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@1,500&display=swap');
-        </style>
-        <Header/>
-        <div>
-            <VideoSection src={MotionGraphics}/>
-            <div style={{}}>
-              <DesignImagesSection/>
-            </div>
-            
-          
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@1,500&display=swap');
+      </style>
+      <Header />
+      <div>
+        <VideoSection src={MotionGraphics} />
+        <div style={{}}>
+          <DesignImagesSection />
         </div>
+      </div>
 
-        <hr style={{margin: "50px 200px 50px 200px"}}/>
-        
-        
+      <hr style={{ margin: "50px 200px 50px 200px" }} />
     </div>
-    
   );
 }
 
