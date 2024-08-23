54980dd91a44386a252793a71f5f841e12b35d8c
Debayan Biswas <dbiswas2001@gmail.com>
2024-08-23 12:38:04 +0530

    update folde structure

--- src/Section/DesignImages/DesignImages.jsx
+++ src/Section/DesignImages/DesignImages.jsx
@@ -1,41 +1,47 @@
 import React from "react";
-import "./DesignImagesCss.css"
+import "./DesignImagesCss.css";
 import Images from "../../Components/Images/Images";
-import VideoSection from "../../Components/VideoSection/VideoSection.jsx"
-import img1 from "../../Assets/Image/lionelmessi.png"
-import img2 from "../../Assets/Image/nascar.png"
-import img3 from "../../Assets/Image/elclasico.png"
-import img4 from "../../Assets/Image/ronaldo.png"
-import MotionGraphics from "../../Assets/Video/motiongraphics2.mp4"
+import VideoSection from "../../Components/VideoSection/VideoSection.jsx";
+import img1 from "../../assets/image/lionelmessi.png";
+import img2 from "../../assets/image/nascar.png";
+import img3 from "../../assets/image/elclasico.png";
+import img4 from "../../assets/image/ronaldo.png";
+import MotionGraphics from "../../assets/video/motiongraphics2.mp4";
 
-function DesignImages(){
-    return (
-      <div className="row">
-          <div className="column">
-            <Images src={img1}/> 
-            <div style={{marginTop: "5px"}}>
-                <center>
-                    <video style={{width : "98%", borderRadius: "10px", boxShadow : "0px 5px 10px 5px rgb(0 0 0 / 20%)"}} loop autoPlay={true} controls={false} muted>
-                        <source src={MotionGraphics} type="video/mp4"/>
-                    </video>
-                </center>
-                
-            </div> 
-            
-          </div>
-          <div className="column"> 
-            <Images src={img2}/>              
-          </div>
-          <div className="column"> 
-            <Images src={img3}/>              
-          </div>
-          
-          <div className="column"> 
-            <Images src={img4}/>              
-          </div>
+function DesignImages() {
+  return (
+    <div className="row">
+      <div className="column">
+        <Images src={img1} />
+        <div style={{ marginTop: "5px" }}>
+          <center>
+            <video
+              style={{
+                width: "98%",
+                borderRadius: "10px",
+                boxShadow: "0px 5px 10px 5px rgb(0 0 0 / 20%)",
+              }}
+              loop
+              autoPlay={true}
+              controls={false}
+              muted
+            >
+              <source src={MotionGraphics} type="video/mp4" />
+            </video>
+          </center>
+        </div>
+      </div>
+      <div className="column">
+        <Images src={img2} />
+      </div>
+      <div className="column">
+        <Images src={img3} />
+      </div>
 
-      </div>      
-    );
-    
+      <div className="column">
+        <Images src={img4} />
+      </div>
+    </div>
+  );
 }
 export default DesignImages;
