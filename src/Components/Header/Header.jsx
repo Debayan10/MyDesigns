12804baa01ea47073171d17e9598963adcda06a9
Debayan Biswas <dbiswas2001@gmail.com>
2024-08-26 22:04:26 +0530

    portfolio button

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import "./HeaderCss.css";
 
-function Header(){
-    return (
-      <div className="SectionBg">
-        <p className="HeaderStyle">
-            <center> My Designs </center>
-        </p>
+function Header() {
+  return (
+    <div className="SectionBg">
+      <a className="HeaderButton" href="https://debayan-biswas.netlify.app">
+        Portfolio
+      </a>
 
-      </div>      
-    );
-    
+      <p className="HeaderStyle">
+        <center> My Designs </center>
+      </p>
+    </div>
+  );
 }
 export default Header;
